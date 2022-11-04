9c73102594f14019a0af831b913ce4f8e6c084dc
Alexander Ink <89594830+ainkpen@users.noreply.github.com>
2022-11-04 13:51:46 -0400

    Update cutter_v5_pyqt.py

--- cutter_v5_pyqt.py
+++ cutter_v5_pyqt.py
@@ -1,10 +1,11 @@
 import sys
 import time
 import threading
+import RPi.GPIO as GPIO
 import stepper
 from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget, QHBoxLayout
+from PyQt5.QtWidgets import *
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -92,8 +93,8 @@ def partycut_thread(cut_array,speed,ramp):
     rotate.start()
     if shutdown: return
     home()
-    # rotate.join()
-    #clean()
+    rotate.join()
+    clean()
     shutdown=True
 
 def piecut(speed=12800,ramp=.1):
@@ -126,19 +127,128 @@ def piecut_thread(speed,ramp):
     shutdown=True
 
 
+class SettingsWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Settings")
+
+        self.setFixedSize(800, 480)
+
+        flags = Qt.WindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
+        self.setWindowFlags(flags)
+
+        self.setCursor(Qt.BlankCursor)
+
+        # Layout
+        layout = QHBoxLayout()
+
+        # Kill Button
+
+        sider_layout = QVBoxLayout()
+
+        home_screen = QPushButton("HOME")
+        home_screen.setObjectName("sideButton")
+        home_screen.clicked.connect(self.home_screen_clicked)
+        sider_layout.addWidget(home_screen)
+
+        wifi_screen = QPushButton("WIFI")
+        wifi_screen.setObjectName("sideButton")
+        sider_layout.addWidget(wifi_screen)
+
+        help_screen = QPushButton("HELP")
+        help_screen.setObjectName("sideButton")
+        sider_layout.addWidget(help_screen)
+
+        sider_layout.addStretch()
+
+        layout.addLayout(sider_layout)
+
+        config_layout = QVBoxLayout()
+
+        tabs = QTabWidget()
+
+        self.tab1 = QWidget()
+        self.tab2 = QWidget()
+        self.tab3 = QWidget()
+
+        tabs.addTab(self.tab1, "DIAL")
+        tabs.addTab(self.tab2, "SLIDER")
+        tabs.addTab(self.tab3, "MISC")
+
+        self.dial_tab()
+        self.slider_tab()
+        self.misc_tab()
+
+        config_layout.addWidget(tabs)
+
+        layout.addLayout(config_layout, stretch=1)
+
+        self.setLayout(layout)
+
+    def dial_tab(self):
+        dial_layout = QHBoxLayout()
+
+        dial = QDial()
+        dial.setNotchesVisible(True)
+        dial.setValue(50)
+        dial.setMinimum(0)
+        dial.setMaximum(100)
+
+        dial_layout.addWidget(dial)
+
+        self.tab1.setLayout(dial_layout)
+
+    def slider_tab(self):
+        slider_layout = QHBoxLayout()
+
+        slider1 = QSlider()
+        slider_layout.addWidget(slider1)
+
+        slider2 = QSlider()
+        slider_layout.addWidget(slider2)
+
+        slider3 = QSlider()
+        slider_layout.addWidget(slider3)
+
+        self.tab2.setLayout(slider_layout)
+
+    def misc_tab(self):
+        misc_layout = QVBoxLayout()
+
+        date_time = QDateTimeEdit()
+        misc_layout.addWidget(date_time)
+
+        line_edit = QLineEdit()
+        misc_layout.addWidget(line_edit)
+
+        spin_box = QSpinBox()
+        misc_layout.addWidget(spin_box)
+
+        prog_bar = QProgressBar()
+        misc_layout.addWidget(prog_bar)
+
+        self.tab3.setLayout(misc_layout)
+
+    def home_screen_clicked(self):
+        self.hide()
+
+
 # Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.settings = SettingsWindow()
+
         self.setWindowTitle("Sm^rt Cutter")
 
         self.setFixedSize(800, 480)
 
-        self.setCursor(Qt.BlankCursor)
-        
         flags = Qt.WindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
         self.setWindowFlags(flags)
+
+        self.setCursor(Qt.BlankCursor)
 
         # Layout
         layout = QVBoxLayout()
@@ -211,6 +321,7 @@ class MainWindow(QMainWindow):
 
         settings_button = QPushButton("\u2699_\u2699")
         settings_button.setObjectName("defaultButton")
+        settings_button.clicked.connect(lambda checked: self.toggle_window(self.settings))
         settings_button.setFixedSize(250,90)
         misc_layout.addWidget(settings_button)
 
@@ -219,7 +330,8 @@ class MainWindow(QMainWindow):
         # Footer
 
         footer_label = QLabel("SM^RT Cutter | version b.5.0 \t\t\t\t\t\t Ag\u00e1pe Automation 2022")
-        footer_label.setAlignment(Qt.AlignBottom)
+        footer_label.setObjectName("footer")
+        footer_label.setAlignment(Qt.AlignHCenter | Qt.AlignBottom)
         layout.addWidget(footer_label)
 
         widgets = QWidget()
@@ -231,6 +343,12 @@ class MainWindow(QMainWindow):
     def kill_screen_clicked(self):
         self.close()
 
+    def toggle_window(self, window):
+        if window.isVisible():
+            window.hide()
+        else:
+            window.show()
+
 
 app = QApplication(sys.argv)
 
@@ -239,14 +357,18 @@ app.setStyleSheet("""
         font-size: 40px;
         font-family: Helvetica;
     }
+    QLabel#footer {
+        text-align: center;
+    }
     QPushButton#sizeButton {
-        background-color: limegreen;
+        background-color: #32CD32;
         font-size: 50px;
         font-family: Helvetica;
         font-weight: bold;
         border-radius: 15px;
-        border-color: black;
-        border-width: 1px;
+        border-color: #2db32d;
+        border-width: 2px;
+        border-style: outset;
     }
     QPushButton#sizeButton::pressed {
         background-color: lime;
@@ -257,8 +379,9 @@ app.setStyleSheet("""
         font-family: Helvetica;
         font-weight: bold;
         border-radius: 15px;
-        border-color: black;
-        border-width: 1px;
+        border-color: #d1d1d1;
+        border-width: 2px;
+        border-style: outset;
     }
     QPushButton#defaultButton::pressed {
         background-color: #F0F0F0;
@@ -269,11 +392,75 @@ app.setStyleSheet("""
         font-family: Helvetica;
         font-weight: bold;
         border-radius: 15px;
-        border-color: black;
-        border-width: 1px;
+        border-color: #cc0202;
+        border-width: 2px;
+        border-style: outset;
     }
     QPushButton#stopButton::pressed {
         background-color: #EE4B2B;
+    }
+    QPushButton#sideButton {
+        background-color: #E8E8E8;
+        font-size: 50px;
+        font-family: Helvetica;
+        border-color: #d1d1d1;
+        border-width: 2px;
+        border-style: outset;
+    }
+    QTabWidget {
+        font-size: 20px;
+        font-family: Helvetica;
+    }
+    QSlider::groove:horizontal {
+        border: 1px solid #bbb;
+        background: white;
+        width: 10px;
+        border-radius: 4px;
+    }
+    
+    QSlider::sub-page:vertical {
+        background: QLinearGradient( x1: 0, y1: 0, x2: 1, y2: 0, stop: 0 #fff, stop: 0.4999 #eee, stop: 0.5 #ddd, stop: 1 #eee );
+        border: 1px solid #777;
+        width: 10px;
+        border-radius: 4px;
+    }
+    
+    QSlider::add-page:vertical {
+        background: QLinearGradient( x1: 0, y1: 0, x2: 1, y2: 0, stop: 0 #78d, stop: 0.4999 #46a, stop: 0.5 #45a, stop: 1 #238 );
+        border: 1px solid #777;
+        width: 10px;
+        border-radius: 4px;
+    }
+    
+    QSlider::handle:vertical {
+        background: qlineargradient(x1:0, y1:0, x2:1, y2:1, stop:0 #eee, stop:1 #ccc);
+        border: 1px solid #777;
+        height: 13px;
+        margin-top: -2px;
+        margin-bottom: -2px;
+        border-radius: 4px;
+    }
+    
+    QSlider::handle:vertical:hover {
+        background: qlineargradient(x1:0, y1:0, x2:1, y2:1, stop:0 #fff, stop:1 #ddd);
+        border: 1px solid #444;
+        border-radius: 4px;
+    }
+    
+    QSlider::sub-page:vertical:disabled {
+        background: #bbb;
+        border-color: #999;
+    }
+    
+    QSlider::add-page:vertical:disabled {
+        background: #eee;
+        border-color: #999;
+    }
+    
+    QSlider::handle:vertical:disabled {
+        background: #eee;
+        border: 1px solid #aaa;
+        border-radius: 4px;
     }
 """)
 
